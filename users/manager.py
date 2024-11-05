3d844e82f272ed533700c18ac9a7e3c018fbebe5
preciousimo <preciousimoniakemu@gmail.com>
2024-11-05 10:42:17 -0800

    "Added input validation for email and password in create_superuser method"

--- users/manager.py
+++ users/manager.py
@@ -11,8 +11,13 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not password:
+            raise ValueError('The Password field must be set')
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
         return self.create_user(email, password, **extra_fields)
 
