d6590e49a1fbbe95801ffcb96e9b5635c4ec55a4
preciousimo <preciousimoniakemu@gmail.com>
2024-11-05 10:59:36 -0800

    "Refactored URL patterns in urecovery/urls.py and users/urls.py"

--- urecovery/urls.py
+++ urecovery/urls.py
@@ -3,14 +3,15 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from users.views import UserViewSet, CustomTokenObtainPairView, CustomTokenRefreshView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Authentication URL patterns
-    path('api/auth/register/', UserViewSet.as_view({'post': 'register'}), name='register'),
-    path('api/auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain'),
-    path('api/auth/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', include([
+        path('register/', UserViewSet.as_view({'post': 'register'}), name='register'),
+        path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain'),
+        path('refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    ])),
 
     # Other app URL patterns
     path('api/', include('users.urls')),
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
-    
-    path('users', views.UserViewSet.as_view({'get': 'list'}), name='user-list'),
+    path('users/', views.UserViewSet.as_view({'get': 'list'}), name='user-list'),
     path('user/<int:pk>/', views.UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user-detail'),
 ]
